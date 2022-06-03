ed34bc19814e6a48317746a6f711f8bc4fed14ac
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-03 00:56:50 -0700

    add #create action and #product_params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,18 @@ class ProductsController < ApplicationController
     render :new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+
+    private
+    def product_params
+      params.require(:product).permit(:name, :cost, :country_of_origin)
+    end
+  end
+
 end
