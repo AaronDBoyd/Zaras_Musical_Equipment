6d374893484d150adfaeb725d423645194073314
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-03 16:06:09 -0700

    add #show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,11 @@ class ProductsController < ApplicationController
       render :edit
     end
 
+    def show
+      @product = Product.find(params[:id])
+      render :show
+    end
+
     private
     def product_params
       params.require(:product).permit(:name, :cost, :country_of_origin)
