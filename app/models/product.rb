bdbc0275326c22e9fcd76922046fb7344dc202a2
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-04 15:05:27 -0700

    add most_reviews scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,13 @@ class Product < ApplicationRecord
 
   scope :last_three, -> { order(created_at: :desc).limit(3) }
   scope :usa, -> { where(country_of_origin: "USA") }
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
 
   validates :name, presence: true
   validates :cost, presence: true
