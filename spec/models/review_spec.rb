7ad2d38afae209dae42019a8c891b9660d178872
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-03 20:03:23 -0700

    pass titleize review.author spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,12 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should validate_length_of(:content).is_at_least(50).is_at_most(250)}
 
+  it("titleizes the author of a review") do
+    product = Product.create({name: "distortion pedal", cost: 40, 
+                              country_of_origin:"japan"})
+    review = Review.create({author: "some person", 
+      content: "abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz",
+                                  rating: 4, product_id: product.id})
+    expect(review.author()).to(eq("Some Person"))
+  end
 end
