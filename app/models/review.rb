d2034b5ffffaf36c6c1e534f32cc13cb25af967f
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-03 19:40:48 -0700

    add length of :content validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,12 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content, presence: true
   validates :rating, presence: true
+  validates_length_of :content, within: 50..250, too_short: "Leave a longer review.", too_long: "Review is too long."
+
+  before_save(:titleize_review)
+
+  private
+    def titleize_review
+      self.author = self.author.titleize
+    end
 end
