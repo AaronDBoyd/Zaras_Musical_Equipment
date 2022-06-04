45bf4a2f7608fb06ba06bb4c03ab680f2445e220
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-04 02:24:19 -0700

    add usa scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   scope :last_three, -> { order(created_at: :desc).limit(3) }
+  scope :usa, -> { where(country_of_origin: "USA") }
 
   validates :name, presence: true
   validates :cost, presence: true
