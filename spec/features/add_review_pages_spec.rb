9c24d5c5406d21fd10281615a11618d20789f56d
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-04 03:43:35 -0700

    pass add review error spec

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 describe "the add a review process" do
-  it "adds a new review" do
+  before :each do
     product = Product.create({name: "Guitar", cost: 40, country_of_origin: 'Japan'})
     visit products_path
     click_on 'Guitar'
+  end
+
+  it "adds a new review" do
     click_link 'Add a review'
     fill_in 'Author', :with => 'some person'
     fill_in 'Content', :with => 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
@@ -14,9 +17,9 @@ describe "the add a review process" do
     expect(page).to have_content 'Some Person'
   end
 
-  # it "gives an error when no name is entered" do
-  #   visit new_product_path
-  #   click_on 'Create Review'
-  #   expect(page).to have_content "Name can't be blank"
-  # end
+  it "gives an error when no name is entered" do
+    click_on 'Add a review'
+    click_on 'Create Review'
+    expect(page).to have_content "Author can't be blank"
+  end
 end
