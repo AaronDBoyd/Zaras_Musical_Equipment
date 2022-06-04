1e87e76579eecdf556475253af37203a6b9503f9
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-03 18:49:18 -0700

    add :country_of_origin validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :cost, presence: true
-  
+  validates :country_of_origin, presence: true
 end
