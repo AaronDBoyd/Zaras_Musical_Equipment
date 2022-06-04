c1584d96b4dccdebb498058bfd5bf08ea99d1ee9
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-04 03:27:51 -0700

    pass edit product spec

---
+++ spec/features/update_product_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'the edit a product process' do
+  it 'edits a product' do
+    product = Product.create({name: "Guitar", cost: 40, country_of_origin: 'Japan'})
+    visit products_path
+    click_on "Guitar"
+    click_on 'Edit'
+    fill_in 'Name', with: "Trumpet"
+    fill_in 'Cost', with: '868'
+    fill_in 'Country of origin', with: 'Croatia'
+    click_on 'Update Product'
+    expect(page).to have_content "Product successfully updated!"
+  end
+end
