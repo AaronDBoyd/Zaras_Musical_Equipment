98a685b8d2d26dda3c405c86d397a045aabe313f
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-03 19:12:19 -0700

    pass titleize_product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,8 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
 
+  it("titleizes the name of a product") do
+    product = Product.create({name: "distortion pedal", cost: 45, country_of_origin: "japan"})
+    expect(product.name()).to(eq("Distortion Pedal"))
+  end
 end
