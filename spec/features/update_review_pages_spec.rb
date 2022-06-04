06d7fb0815d5130c8b26f2a3df71c66457aba027
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-04 04:00:53 -0700

    pass edit review error spec

--- spec/features/update_review_pages_spec.rb
+++ spec/features/update_review_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'the edit a review process' do
-  it 'edits a review' do
+  before :each do 
     product = Product.create({name: "Guitar", cost: 40, country_of_origin: 'Japan'})
     review = Review.create({author: 'some person', 
       content: 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz',
@@ -10,17 +10,18 @@ describe 'the edit a review process' do
     click_on "Guitar"
     click_on 'Some Person'
     click_on 'Edit review'
+  end
+
+  it 'edits a review' do
     fill_in 'Author', with: "Another person"
-    # fill_in 'Cost', with: '868'
-    # fill_in 'Country of origin', with: 'Croatia'
     click_on 'Update Review'
     expect(page).to have_content "Review successfully updated!"
     expect(page).to have_content "Another Person"
   end
 
-  # it "gives an error when no name is entered" do
-  #   visit new_product_path
-  #   click_on 'Create Product'
-  #   expect(page).to have_content "Name can't be blank"
-  # end
+  it "gives an error when no name is entered" do
+    fill_in 'Author', with: ""
+    click_on 'Update Review'
+    expect(page).to have_content "Author can't be blank"
+  end
 end
