0f8f6c0bf53f7c4f2574f901b4c7535a81e0f7e9
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-11 02:25:58 -0700

    edit home action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
   end
 
   def index
-    @products = Product.all
+    @products = Product.all.paginate(:page => params[:page], per_page:10)
     render :index
   end
 
