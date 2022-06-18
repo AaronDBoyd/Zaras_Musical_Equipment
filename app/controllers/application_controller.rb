0c1b257ed49b689bebd0057c49b5cb0b95811ce0
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-18 03:36:28 -0700

    edit flash alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def authorize
     if !current_user
-      flash[:alert] = "You aren't authorized to visit that page."
+      flash[:alert] = "You aren't authorized to visit that page. Please Sign In."
       redirect_to '/'
     end
   end
