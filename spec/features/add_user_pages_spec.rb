e1fb2eca59d92cd7f2a5409b8b4a2cd714adc60d
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-18 05:56:53 -0700

    add and pass user sign in spec

--- spec/features/add_user_pages_spec.rb
+++ spec/features/add_user_pages_spec.rb
@@ -11,4 +11,14 @@ describe "the add a user product" do
     expect(page).to have_content "You've successfully signed up!"
     expect(page).to have_content 'Welcome, User@gmail.com'
   end
+  
+  it "signs a user in" do
+    User.create({email: 'admin', password: "123456", admin: true})
+    visit signin_path
+    fill_in 'Email', with: 'admin'
+    fill_in 'Password', with: '123456'
+    click_on 'Sign in'
+    expect(page).to have_content "You've signed in."
+    expect(page).to have_content 'Welcome, Admin'
+  end
 end
