a4ca5a28a78f960b5d01765df0b0e4c43414a11e
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-18 06:08:54 -0700

    add sign in user to spec

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 describe "the delete a product process" do
   it "deletes a product" do
+    User.create({email: "admin", password: "123456", admin: true})
+    visit signin_path
+    fill_in 'Email', with: 'admin'
+    fill_in 'Password', with: '123456'
+    click_on 'Sign In'
     product = Product.create({name: "Guitar", cost: 40, country_of_origin: 'Japan'})
     visit products_path
     click_on 'Guitar'
